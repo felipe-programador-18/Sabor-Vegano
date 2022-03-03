5413136d291f1ece82d3b6ac99f45727ed175316
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-03 14:41:52 -0300

    c3-create design

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Home = () =>{
     return <>
-    <div className=''>
-    <p className='text-center italic'>Home</p>
-    <p className='text-center italic'>Home</p>
 
+    <div className='mx-auto bg-red-500 text-center shadow-md p-4'>
+       
+      <Link  href='/'>
+        <a className='italic m-4'>Home</a>
+      </Link>
 
-
-
-
-
-
-
-    
-    </div>
-    
+      <Link href='/pedido'>
+      <a className='italic' >Pedidos</a>
+      </Link>
+         
+     </div>
+          
+     <div className='flex flex-start justify-center opacity-100'>
+     
+     <div className='bg-green-300 w-48 h-48 m-4'> <img src='file.jpg'></img>  </div>
+     <div className='bg-blue-300 w-48 h-48 m-4'> <img  src='sushi.jpg'></img> </div>
+     <div className='bg-violet-700 w-48 h-48 m-4'>  <img  src='vege.jpg'></img> </div>
+     <div className='bg-yellow-500 w-48 h-48 m-4'>  <img src='suco.jpg'></img> </div>
+   </div>
+       
     
     </>
 }
