210f405ba77fd6e738d32dab0265b2b8469c89ae
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-03 11:43:44 -0300

    c1- adding all

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import "/styles/globals.css"
+
 const Home = () =>{
     return <>
-    <h1 className='italic'>teste here more </h1>
+    <h1 className='text-center italic'>teste here more </h1>
     </>
 }
 
