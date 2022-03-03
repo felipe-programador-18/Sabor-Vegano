02f5a4f6a22fa8b6828deeab3fc2b55565dac9cd
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-03 12:48:57 -0300

    c2-adding p div

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () =>{
     return <>
-    <h1 className='text-center italic'>teste here more </h1>
+    <div className=''>
+    <p className='text-center italic'>Home</p>
+    <p className='text-center italic'>Home</p>
+
+
+
+
+
+
+
+
+    
+    </div>
+    
+    
     </>
 }
 
