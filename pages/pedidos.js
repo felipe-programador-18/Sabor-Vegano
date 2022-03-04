838ca8d43c8a04e4dc92a7be56b17c8256321007
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-04 12:26:30 -0300

    c8- adding page and set up pages

--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
+
+
+const Option = (props) =>{
+    return(
+        <option>
+        {props.menu}
+        </option>
+        )
+}
+
+const Para = (props) =>{
+    return(
+        <p>{props.text}</p>
+    )
+}
+
+
 const Pedidos = () => {
+    
+    const [seleciona, setseleciona] = useState('')
     return <>
-       <div className="mx-auto text-center inline">
-       <h1>Pedidos</h1>
+    
+    
+    <div className="mx-auto text-center bg-blue-400 opacity-75">
+       <Link href='/'>
+           <img id="icone" src="icon.jpg.jpg"></img>
+      </Link>
+          <h1 className="bold text-lg">Sabor Vegano</h1>
+       
+    </div> 
+       
+
+    
+       <div className="w-48 mx-auto py-4">
+       <h1 className="font-bold">Pedidos</h1>
 
         <form  method="get" action="formulario">
-        <input className="block" type='text' placeholder="nome"></input>
-        <input className="inline" type='email' placeholder="e-mail"></input>
-        <input className="inline" type='number' placeholder="Digite seu cpf"></input>
-        <input className="inline" type='number' placeholder="Celular"></input>
-        <input className="inline" type='text' placeholder="Endereço"></input>
+        <label className="font-bold text-center">Seu Nome</label>
+        <input className="bg-blue-100 p-4 rounded-lg block py-4 m-3" type='text' placeholder="nome"></input>
+        
+        <label className="font-bold text-center">E-mail</label>
+        <input className="bg-blue-100 p-4 rounded-lg block py-4 m-3" type='email' placeholder="e-mail"></input>
+        
+        <label className="font-bold text-center">Whatsaap</label>
+        <input className="bg-blue-100 p-4 rounded-lg block py-4 m-3" type='number' placeholder="Celular"></input>
+       
+        <label className="font-bold text-center">Endereço</label>
+        <input className="bg-blue-100 p-4 rounded-lg block py-4 m-3" type='text' placeholder="Endereço"></input>
         </form>   
 
+         
+         <h1>Selecione Pedido:</h1>
+        <select className="m-4" one={seleciona} onChange={e=>setseleciona(e.target.value)}>
+         <Option  menu='Super Vegano: R$:35.90 ' />
+         <Option  menu='Hamburger Vegano:  R$:45.90 ' />
+         <Option  menu='Picadão Vegano: R$:65.90 ' />
+         
+         </select>
+           <Para text= 'Voce selecionou:' />
+           {seleciona}
+
        </div>
+       
+    
+       
+       
        </>
 }
 
