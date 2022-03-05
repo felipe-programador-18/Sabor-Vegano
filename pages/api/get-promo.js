cfac22097a45aa5531e7420803ce46223839bd76
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-04 21:44:37 -0300

    c17- adding api with coupon

---
+++ pages/api/get-promo.js
@@ -0,0 +1,9 @@
+
+// remember here i get Api coupon
+//create message coupon
+export default async(req, res) =>{
+    res.end(JSON.stringify({
+        showCoupon: false,
+        mensagem:'Olá Vegamania, tudo bem?'
+    }))
+}
