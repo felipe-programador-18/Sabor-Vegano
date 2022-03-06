6e7f543553d2814bdf08652a194a5bd3ca450451
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-05 21:55:51 -0300

    c35- create date to getting acess in spreadsheet

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,29 @@
+import  { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+// here to connect spreadsheet!!!
+
+const doc = new GoogleSpreadsheet('1NDvbzZlWgP7UjnEsdkcC6SJIpAzjKshjZ_gDo-4CQoI')
+
+
+
 export default async(req, res) =>{
-    console.log(JSON.parse(req.body))
-    res.end(req.body)
+    try{
+        await doc.useServiceAccountAuth(credentials)
+        await doc.loadInfo()
+        //Nome	Email	Whatsaap	Cupon	Promo
+        const sheet = doc.sheetsByIndex[1]
+        const data = JSON.parse(req.body)
+         await sheet.addRow({
+             Nome: data.Nome,
+             Email: data.Email,
+             Whatsaap: data.Whatsaap,
+             Cupon: data.Cupon,
+             Promo: data.Promo,
+         })
+         res.end(req.body)
+          } catch (err) {
+            console.log(err)
+            res.end('error')
+          }
+
 }
